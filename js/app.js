4a342e2425d54aaf49e1ca70a74dfec8b78e52b8
Volkov Arkadii <arcadevovk@gmail.com>
2018-04-18 23:50:05 +0300

    Add displaySymbol function to event listener

--- js/app.js
+++ js/app.js
@@ -34,12 +34,18 @@ function shuffle(array) {
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, digsplay a message with the final score (put this functionality in another function that you call from this one)
  */
 
 
-const deck = document.querySelector(".deck")
+const deck = document.querySelector(".deck");
 
-deck.addEventListener("click", function() {
+function displaySymbol(event) {
+  event.target.classList.add("open","show")
+};
 
+deck.addEventListener("click", function(event) {
+  if (event.target.className === "card") {
+    displaySymbol(event);
+  }
 })
